ec92f6d88ca54429627591bc441eb143fb5d7ded
Timo <timo.schessl@gmx.de>
2020-05-10 21:30:03 +0200

    Creating account works

---
+++ src/post.js
@@ -0,0 +1,85 @@
+function checkIfUsernameExists() {
+
+    var username = document.getElementById("username").value;
+
+    fetch("http://localhost:9000/.netlify/functions/server/")
+        .then(result => {
+            result.text().then(text => {
+
+                var flag = false
+
+                var jsonObject = JSON.parse(text)
+
+                jsonObject.forEach(item => {
+
+                    if (!(typeof item.username === 'undefined')) {
+                        if (item.username == username) {
+
+                            flag = true
+                            alert("This username already exists!")
+                            
+                        }
+                    }
+
+                })
+
+            postData(flag)
+
+            })
+        })
+
+}
+
+
+
+function postData(flag) {
+
+    if(flag)
+    {
+        return false
+    }
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var repeatedPassword = document.getElementById("repeat").value;
+
+        if (username.length >= 4) {
+
+            if (password.length >= 6) {
+
+
+                if (password == repeatedPassword) {
+                    fetch("http://localhost:9000/.netlify/functions/server/post", {
+                        method: 'POST',
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({
+
+                            username: username,
+                            password: password
+                        })
+
+                    })
+                        .then(result => {
+                            console.log(result)
+                            result.text().then(text => { document.getElementById("showData").value = text })
+                        })
+                    alert("Created new account successfully!")
+                }
+                else {
+                    alert("The passwords are not the same!")
+                }
+
+            }
+            else {
+                alert("Please enter at least 6 characters for the password!")
+            }
+
+        }
+        else {
+            alert("Please enter at least 4 characters for your username!")
+        }
+ 
+}
+
+
+
--- src/server.js
+++ src/server.js
@@ -2,12 +2,59 @@ const express = require('express')
 const serverless = require('serverless-http')
 const server = express()
 const router = express.Router()
+const mongoose = require('mongoose')
+const PostModel = require('./PostModel')
+const bodyParser = require("body-parser")
+const cors = require("cors")
+URL = "mongodb+srv://mainuser:test@cluster-0gtou.mongodb.net/test?retryWrites=true&w=majority"
 
-router.get("/", (req,res)=>
+server.use(cors())
+server.use(bodyParser.json())
+
+mongoose.connect(URL,
+    { useNewUrlParser: true , useUnifiedTopology: true}, () =>
+    {
+        console.log("Connected with database successfull!")
+    })
+
+router.get("/", async (request, response)=>
 {
-    res.json({"Hello":"Hi! New content"})
-}
-)
+     try{
+     const data = await PostModel.find()
+     console.log(data)
+     response.json(data)
+     console.log("Get request successful.")
+     }
+     catch(error)
+     {
+          console.log(error)
+          response.json(error)
+     } 
+    
+})
+
+router.post("/post", (request, response) => {
+    console.log("The post request is: " + request.body)
+
+    const postaction = new PostModel({
+        username: request.body.username,
+        password: request.body.password
+    })
+
+
+    postaction.save().then(result => {
+        response.json(result)
+        console.log(result)
+        console.log("Post request successful.")
+    })
+        .catch(error => {
+
+            response.json(error)
+            console.log(error)
+        })
+
+
+})
 
 
 server.use("/.netlify/functions/server",router)
