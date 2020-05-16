f47e28c635c310437648d8f11a89880f8a390c3d
Timo <timo.schessl@gmx.de>
2020-05-16 13:03:39 +0200

    Set required to true again in postModel

--- src/PostModel.js
+++ src/PostModel.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose")
 const PostPattern = mongoose.Schema({
     username: {
        type: String,
-       required:false
+       required:true
     },
     password: {
         type: String,
-        required:false
+        required:true
      },
      email:{
         type:String,
-        required:false
+        required:true
      }
 })
 
